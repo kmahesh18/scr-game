3b7a65734b8c3c299760521d9ac149368e4c8c02
kmahesh18 <maheshkolli888@gmail.com>
2025-07-26 18:01:46 +0000

    #1

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './components/Hero'
 import About from './components/About'
 import Navbar from './components/Navbar'
 import Features from './components/Features'
+import GamesGallery from './components/GamesGallery'
 import Story from './components/Story'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
@@ -16,6 +17,7 @@ const App = () => {
    <Navbar/>
     <About/>
     <Features/>
+    <GamesGallery/>
     <Story/>
     <Contact/>
     <Footer/>
